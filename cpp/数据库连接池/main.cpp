6f22271872aa5dd95607d9ae6690d48cfee0bf9b
corner <corner@88.com>
2024-06-16 14:30:15 +0800

    连接池 重新测试

--- cpp/数据库连接池/main.cpp
+++ cpp/数据库连接池/main.cpp
@@ -1,13 +1,20 @@
 #include "CommonConnectionPool.h"
 #include "Connection.h"
 
+/*
+ * 单线程普通连接测试，耗时： 286550ms
+ * 单线程连接池测试，耗时：   192328ms
+ * 多线程普通连接测试，耗时： 360111ms
+ * 多线程连接池测试，耗时：   125513ms
+ */
+
 int main() {
-  /*
-  // 多线程普通连接测试，耗时：160649ms
+  // /*
+  // 多线程普通连接测试，耗时： 360111ms
   Connection conn;
   conn.connect("localhost", 3306, "root", "1", "chat");
   clock_t start = clock();
-  thread t1([&]() {
+  thread t1([]() {
     for (int i = 0; i < 250; ++i) {
       Connection conn;
       char sql[1024] = {0};
@@ -18,29 +25,35 @@ int main() {
     }
   });
 
-  thread t2([&]() {
+  thread t2([]() {
     for (int i = 0; i < 250; ++i) {
+      Connection conn;
       char sql[1024] = {0};
       sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
               "zhang san", 20, "male");
+      conn.connect("localhost", 3306, "root", "1", "chat");
       conn.update(sql);
     }
   });
 
-  thread t3([&]() {
+  thread t3([]() {
     for (int i = 0; i < 250; ++i) {
+      Connection conn;
       char sql[1024] = {0};
       sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
               "zhang san", 20, "male");
+      conn.connect("localhost", 3306, "root", "1", "chat");
       conn.update(sql);
     }
   });
 
-  thread t4([&]() {
+  thread t4([]() {
     for (int i = 0; i < 250; ++i) {
+      Connection conn;
       char sql[1024] = {0};
       sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
               "zhang san", 20, "male");
+      conn.connect("localhost", 3306, "root", "1", "chat");
       conn.update(sql);
     }
   });
@@ -51,9 +64,9 @@ int main() {
   t4.join();
   cout << "time: " << clock() - start << "ms" << endl;
   return 0;
-  */
+  // */
 
-  // 多线程连接池测试，耗时：125148ms
+  // 多线程连接池测试，耗时：125513ms
   /*
   clock_t start = clock();
   thread t1([]() {
@@ -107,23 +120,23 @@ int main() {
   return 0;
   */
 
-  //********************** 普通连接测试，耗时：73244ms **********************
+  //********************** 普通连接测试，耗时：286550ms **********************
   /*
-  Connection conn;
-  conn.connect("localhost", 3306, "root", "1", "chat");
-
   clock_t start = clock();
   for (int i = 0; i < 1000; ++i) {
+    Connection conn;
     char sql[1024] = {0};
     sprintf(sql, "insert into user(name,age,sex) values('%s',%d,'%s')",
             "zhang san", 20, "male");
+    conn.connect("localhost", 3306, "root", "1", "chat");
     conn.update(sql);
   }
   cout << "time: " << clock() - start << "ms" << endl;
   return 0;
   */
 
-  //******************** 线程池连接测试，耗时：194060ms ******************
+  //******************** 连接池连接测试，耗时：192328ms ******************
+  /*
   clock_t start = clock();
   ConnectionPool *cp = ConnectionPool::getConnectionPool();
   for (int i = 0; i < 1000; ++i) {
@@ -135,4 +148,5 @@ int main() {
   }
   cout << "time: " << clock() - start << "ms" << endl;
   return 0;
+  */
 }
